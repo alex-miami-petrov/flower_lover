1db968b9ebb6776ee474594e2ac947d8ffd8f34c
Miami <alex.miami.petrov@gmail.com>
2024-10-27 09:28:42 +0200

    m

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Container from "../Container/Container";
+import logo from "../../img/logo/logo.png";
+import icons from "../../img/icons.svg";
+import s from "./Header.module.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={s.header}>
+      <Container>
+        <div className={s.headWrap}>
+          <img
+            src={logo}
+            alt="logo"
+            className={s.headLogo}
+            width="126"
+            height="63"
+          />
+
+          <div className={s.headBtnWrap}>
+            <button type="button" className={s.headSearchButton}>
+              <svg className={s.headSearchIcon} width="22" height="23">
+                <use href={`${icons}#icon-search`} />
+              </svg>
+            </button>
+
+            <button type="button" className={s.burger}>
+              <svg className={s.burgerIcon} width="42" height="42">
+                <use href={`${icons}#icon-burger`} />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/Components/Header/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="headNav">
+        <ul className="navList">
+          <li className="navItem">
+            <a href="" className="navLink"></a>
+          </li>
+          <li className="navItem">
+            <a href="" className="navLink"></a>
+          </li>
+          <li className="navItem">
+            <a href="" className="navLink"></a>
+          </li>
+          <li className="navItem">
+            <a href="" className="navLink"></a>
+          </li>
+          <li className="navItem">
+            <a href="" className="navLink"></a>
+          </li>
+          <li className="navItem">
+            <a href="" className="navLink"></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
