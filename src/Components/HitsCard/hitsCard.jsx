fcffc9ed09926b19be87f13d10d93129de3f9930
Miami <alex.miami.petrov@gmail.com>
2024-11-18 13:26:01 +0200

    m

--- src/Components/HitsCard/hitsCard.jsx
+++ src/Components/HitsCard/hitsCard.jsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
-const HitsCard = ({ productId }) => {
-  const [product, setProduct] = useState(null);
-  const [error, setError] = useState(null);
+// const HitsCard = ({ productId }) => {
+//   const [product, setProduct] = useState(null);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:5000/product/${productId}`
-        );
-        setProduct(response.data);
-      } catch (err) {
-        setError("Error fetching product");
-        console.error("Error fetching product:", err);
-      }
-    };
+//   useEffect(() => {
+//     const fetchProduct = async () => {
+//       try {
+//         const response = await axios.get(
+//           `http://localhost:5000/product/${productId}`
+//         );
+//         setProduct(response.data);
+//       } catch (err) {
+//         setError("Error fetching product");
+//         console.error("Error fetching product:", err);
+//       }
+//     };
 
-    fetchProduct();
-  }, [productId]);
+//     fetchProduct();
+//   }, [productId]);
 
-  if (error) {
-    return <div>{error}</div>;
-  }
+//   if (error) {
+//     return <div>{error}</div>;
+//   }
 
-  if (!product) {
-    return <div>Loading...</div>;
-  }
+//   if (!product) {
+//     return <div>Loading...</div>;
+//   }
 
-  return (
-    <div>
-      <p>{product.price}</p>
-      <h1>{product.name}</h1>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <p>{product.price}</p>
+//       <h1>{product.name}</h1>
+//     </div>
+//   );
+// };
 
-export default HitsCard;
+// export default HitsCard;
