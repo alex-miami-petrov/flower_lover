a38d1634b90fe6db381d8573829f8059b4409d8d
Miami <alex.miami.petrov@gmail.com>
2024-12-01 11:10:43 +0200

    m

--- src/Components/Hits/hits.jsx
+++ src/Components/Hits/hits.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-// import ProductCard from "../ProductCard/productCard.jsx";
 import s from "./hits.module.css";
 import h1 from "../../img/Hits/h1.png";
 import h2 from "../../img/Hits/h2.png";
 import h3 from "../../img/Hits/h3.png";
 
 import HitsSwiper from "../../swiper/hitsSwiper.jsx";
+import ProductCard from "../ProductCard/productCard.jsx";
+import { useState } from "react";
 
 const products = [
   {
@@ -46,10 +47,29 @@ const products = [
 ];
 
 const Hits = () => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1440);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 1440);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div>
       <h2 className={s.hitsTitle}>Хіт продажів</h2>
-      <HitsSwiper products={products} />
+      {isDesktop ? (
+        <div className={s.productGrid}>
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <HitsSwiper products={products} />
+      )}
     </div>
   );
 };
