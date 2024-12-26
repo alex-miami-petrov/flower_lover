9a6bd3116d5b995014d5d73235d947b97048c14c
Miami <alex.miami.petrov@gmail.com>
2024-12-26 11:42:30 +0200

    no_api

--- src/Components/Hits/hitsDesktop.jsx
+++ src/Components/Hits/hitsDesktop.jsx
@@ -1,29 +1,3 @@
-// import React, { useState } from "react";
-// import ProductCard from "../ProductCard/productCard.jsx";
-// import s from "./hitsDesktop.module.css";
-
-// const HitsDesktop = ({ products }) => {
-//   const [showAll, setShowAll] = useState(false);
-
-//   const displayedProducts = showAll ? products : products.slice(0, 3);
-//   return (
-//     <>
-//       <div className={showAll ? s.productGridExpanded : s.productGrid}>
-//         {displayedProducts.map((product) => (
-//           <ProductCard key={product.id} product={product} />
-//         ))}
-//       </div>
-//       {!showAll && (
-//         <button className={s.hitsDeskBtn} onClick={() => setShowAll(true)}>
-//           Дивитись всі
-//         </button>
-//       )}
-//     </>
-//   );
-// };
-
-// export default HitsDesktop;
-
 import React, { useState } from "react";
 import ProductCard from "../ProductCard/productCard.jsx";
 import s from "./hitsDesktop.module.css";
@@ -36,7 +10,7 @@ const HitsDesktop = ({ products }) => {
     <>
       <div className={showAll ? s.productGridExpanded : s.productGrid}>
         {displayedProducts.map((product) => (
-          <ProductCard key={product._id} product={product} />
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
       {!showAll && (
@@ -49,3 +23,29 @@ const HitsDesktop = ({ products }) => {
 };
 
 export default HitsDesktop;
+
+// import React, { useState } from "react";
+// import ProductCard from "../ProductCard/productCard.jsx";
+// import s from "./hitsDesktop.module.css";
+
+// const HitsDesktop = ({ products }) => {
+//   const [showAll, setShowAll] = useState(false);
+
+//   const displayedProducts = showAll ? products : products.slice(0, 3);
+//   return (
+//     <>
+//       <div className={showAll ? s.productGridExpanded : s.productGrid}>
+//         {displayedProducts.map((product) => (
+//           <ProductCard key={product._id} product={product} />
+//         ))}
+//       </div>
+//       {!showAll && (
+//         <button className={s.hitsDeskBtn} onClick={() => setShowAll(true)}>
+//           Дивитись всі
+//         </button>
+//       )}
+//     </>
+//   );
+// };
+
+// export default HitsDesktop;
